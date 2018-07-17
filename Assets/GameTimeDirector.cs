cb5f8369d173f2c69ecb10bc2a3708c8c92f0033
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2018-07-18 06:15:31 +0900

    GameTime Display Add
    Add GameTime Display

---
+++ Assets/GameTimeDirector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameTimeDirector : MonoBehaviour {
+
+    GameObject GameTimeObject;
+
+    float Minute;
+    float Second;
+
+    // Use this for initialization
+    void Start () {
+        this.GameTimeObject = GameObject.Find("GameTime");
+        this.Minute = 0;
+        this.Second = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Second += Time.deltaTime;
+        if(Second > 60)
+        {
+            Minute++;
+            Second = Second - 60;
+        }
+
+        GameTimeObject.GetComponent<Text>().text = Minute.ToString("00") + ":" + Second.ToString("00");
+	}
+}
